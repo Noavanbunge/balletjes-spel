1e748604000b2f13fbbaec93f6fcf6907874571e
Noa van <78596993+Noavanbunge@users.noreply.github.com>
2021-09-08 12:33:44 +0000

    class toegevoegd

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-var x, y, v, z;
+var x, y, vx, vz;
 
 function setup() {
 	createCanvas(500, 400);
 
   x = 0;
   y = 200;
-  v = 6;
-  z = 6;
+  vx = 6;
+  vz = 6;
 
 
 }
-
-function draw() {
+class Rectangle {
+ 
+  constructor(x,y,w,h,vx,vy) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.vx = vx;
+  this.vz = vz;
+  }
+}
+function draw() 
+{
 	background(225);
 
   ellipse(x,y,50,50,5,5);
-  x = x + v;
-  y = y + z;
+  x = x + vx;
+  y = y + vz;
 
   if(x <= 0 || x >= 500){
-    v = v * -1;
+    vx = vx * -1;
   }
 
   if(y <= 0 || y >= 400){
-    z = z * -1
+    vz = vz * -1
   }
 }
