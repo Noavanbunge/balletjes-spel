13c2eb49c922c4b6a3fd2e04cf04268a35a2319f
Noa van <78596993+Noavanbunge@users.noreply.github.com>
2021-09-15 12:23:19 +0000

    3 balletjes

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-var x, y, vx, vz;
+
+class Ball {
+  constructor(x, y, w, h, vx, vy, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.vx = vx;
+    this.vy = vy;
+    this.c = c;
+  }
+
+  drawball() {
+    fill(this.c)
+    ellipse(this.x, this.y, 50, 50, 5, 5);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if (this.x <= 0 || this.x >= 500) {
+      this.vx = this.vx * -1;
+    }
+
+    if (this.y <= 0 || this.y >= 400) {
+      this.vy = this.vy * -1
+    }
+  }
+}
+
+
 
 function setup() {
-	createCanvas(500, 400);
+  createCanvas(500, 400);
 
-  x = 0;
-  y = 200;
-  vx = 6;
-  vz = 6;
-
-
+ ball1 = new Ball(30,30,40,40,5,5,"red")
+ ball2 = new Ball(130,40,40,40,5,5,"green")
+ ball3 = new Ball(70,20,40,40,5,5,"blue")
 }
-class Rectangle {
- 
-  constructor(x,y,w,h,vx,vy) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.vx = vx;
-  this.vz = vz;
-  }
-}
-function draw() 
-{
-	background(225);
 
-  ellipse(x,y,50,50,5,5);
-  x = x + vx;
-  y = y + vz;
+function draw() {
+  background(225);
 
-  if(x <= 0 || x >= 500){
-    vx = vx * -1;
-  }
+  ball1.drawball();
+  ball2.drawball();
+  ball3.drawball();
 
-  if(y <= 0 || y >= 400){
-    vz = vz * -1
-  }
+
 }
